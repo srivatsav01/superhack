02794088e2426838b74b63943afd6bd8febc93eb
srivatsav01 <cs20btech11003@iith.ac.in>
2023-08-13 19:24:51 +0530

    Change the flow

--- student-verification-frontend/src/App.js
+++ student-verification-frontend/src/App.js
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReclaimSDK from '@reclaimprotocol/reclaim-client-sdk';
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
-import { getDefaultProvider} from 'ethers';
 import { Web3Provider } from '@ethersproject/providers';
-
+import { Wallet, getDefaultProvider,JsonRpcProvider } from 'ethers';
 import QRCode from 'react-qr-code';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-
-const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Replace with the correct contract address
-const schemaString = "uint256 eventId, uint8 voteIndex"; // Define your schema
-const schemaEncoder = new SchemaEncoder(schemaString);
-const schemaUID = '0x3b17d657b982933abb9a27f563160264784d9bd8afd73b94802dcb4d94b61143'
+const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e";
+const schemaEncoder = new SchemaEncoder("address studentAddr, bool isStudent");
+const schemaUID = '0x3b17d657b982933abb9a27f563160264784d9bd8afd73b94802dcb4d94b61143';
 
 function App() {
-  const [isProofSubmitted, setIsProofsSubmitted] = useState(false);
+  const [userAddress, setUserAddress] = useState(null);
   const [sessionLink, setSessionLink] = useState('');
-  const [proofs, setProofs] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setUserAddress(accounts[0]);
+    } else {
+      alert("MetaMask not detected!");
+    }
+  };
 
   const generateSession = async () => {
     setIsLoading(true);
@@ -29,9 +34,9 @@ function App() {
 
     try {
       const submissionData = await session?.onSubmission;
-      console.log(submissionData)
-      setProofs(submissionData.proofs);
-      setIsProofsSubmitted(true);
+      if (submissionData.proofs) {
+        createAttestation(); // Call the attestation function directly if proofs are submitted
+      }
     } catch (e) {
       console.error('Error during submission:', e);
     } finally {
@@ -39,53 +44,43 @@ function App() {
     }
   };
 
-  const createAttestation = async () => {
+ // WARNING: Do not use a real private key in client-side code!
+
+const createAttestation = async () => {
     const eas = new EAS(EASContractAddress);
+    const YOUR_PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY;
+    console.log(YOUR_PRIVATE_KEY)
+    // Use your private key to create a signer
+    const SEPOLIA_RPC_URL = "https://eth-sepolia-public.unifra.io";
 
-    const provider = new Web3Provider(window.ethereum);
-    // Assuming you have initialized the web3 provider
-let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-const signer = provider.getSigner(accounts[0]); // This gets the signer for the first account
-console.log(signer);
-
-
-
-
-// Connect the signer to the EAS
-eas.connect(signer);
-
-    // 2. Get the signer
+    // Create a provider for Sepolia
+    const provider = new JsonRpcProvider(SEPOLIA_RPC_URL);
     
-    // 3. Connect the signer to the EAS
-    // Initialize SchemaEncoder with the new schema string
-    const schemaEncoder = new SchemaEncoder("address studentAddr, bool isStudent");
+    // Use your private key to create a signer
+    const wallet = new Wallet(process.env.REACT_APP_PRIVATE_KEY);
+    const signer = wallet.connect(provider);
+    eas.connect(signer);
+    console.log(signer)
+
+    // Now, use the signer directly with EAS
     const encodedData = schemaEncoder.encodeData([
-      { name: "studentAddr", value: "0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165", type: "address" },
+      { name: "studentAddr", value: userAddress, type: "address" },
       { name: "isStudent", value: true, type: "bool" },
     ]);
-    
-    const schemaUID = "0x4464cea8669763510204204e367c58716bd9c443af1da43d9cc8c5d238c53b5e";
-                       
 
-    
     const tx = await eas.attest({
       schema: schemaUID,
       data: {
-        recipient: "0x3C7AF4E43e582A4fF5CbAfcB91195b2376DcF954", // This can be the student's address or any other recipient
+        recipient: userAddress,
         expirationTime: 0,
-        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+        revocable: true,
         data: encodedData,
-         // example valu
       },
-     
-    },{ gasLimit: 500000 });
-    
-    const newAttestationUID = await tx.wait();
-    
-    console.log("New attestation UID:", newAttestationUID);
-    
-  };
+    }, { gasLimit: 500000, signer: signer }); // Pass the signer here
 
+    const newAttestationUID = await tx.wait();
+    console.log("New attestation UID:", newAttestationUID);
+};
   return (
     <div className="app-container">
       <header className="app-header">
@@ -93,25 +88,25 @@ eas.connect(signer);
       </header>
       <main className="app-main">
         <div className="verification-box">
-          {isProofSubmitted && (
+          {!userAddress && (
+            <button className="btn" onClick={connectMetaMask}>
+              Connect MetaMask
+            </button>
+          )}
+
+          {userAddress && !sessionLink && (
             <button className="btn" onClick={generateSession} disabled={isLoading}>
               Prove you're a student
             </button>
           )}
 
-          {sessionLink && !isProofSubmitted && (
+          {sessionLink && (
             <div>
               <h2>Scan the QR Code to Verify</h2>
               <div>
                 <QRCode value={sessionLink} />
               </div>
             </div>
-          )}
-
-          {!isProofSubmitted && (
-            <button className="btn" onClick={createAttestation} disabled={isLoading}>
-              Attest on Chain
-            </button>
           )}
         </div>
       </main>
